8cbf5c829219c67ef779a140c981bf903183c064
angsonta <angsontheekan@gmail.com>
2023-06-12 16:21:16 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import Navbar from "./Components/Navbar/Navbar";
+import Banner from "./Components/Banner/Banner";
+import RowPoster from "./Components/RowPoster/RowPoster";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <Navbar/>
+    <Banner/>
+    <RowPoster/>
+   </div>
   );
 }
 
