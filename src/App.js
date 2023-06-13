e8d4c036e1a629affac40814b8b1b62701c6bfd3
angsonta <angsontheekan@gmail.com>
2023-06-13 12:34:18 +0530

    Added some items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import "./App.css"
+import "./App.css";
+import {Action,Comedy,Horror,Originals, Romance} from './urls'
 import Navbar from "./Components/Navbar/Navbar";
 import Banner from "./Components/Banner/Banner";
 import RowPoster from "./Components/RowPoster/RowPoster";
@@ -9,7 +10,11 @@ function App() {
    <div>
     <Navbar/>
     <Banner/>
-    <RowPoster/>
+    <RowPoster url= {Originals}  title='Netflix Originals' />
+    <RowPoster url= {Action}     title='Action Movies' isSmall />
+    <RowPoster url= {Comedy}     title='Comedy Movies' isSmall />
+    <RowPoster url= {Romance}     title='Romance Movies' isSmall />
+    <RowPoster url= {Horror}     title='Horror Movies' isSmall />
    </div>
   );
 }
